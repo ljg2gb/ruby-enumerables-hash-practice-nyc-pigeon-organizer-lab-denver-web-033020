d374dc8b751ed10eb7b0b2baf1be4a91299ec3d7
Lydia Gregory <ljg2gb@virginia.edu>
2020-03-16 23:58:12 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,37 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data[:gender].each do |key, value| 
+    i = 0 
+    while i < value.length do
+    new_hash[value[i]] = {
+      :color => [],
+      :gender => [key.to_s],
+      :lives => []
+    }
+    i += 1
+    end
+   #binding.pry
+   
+  end
+  data[:color].each do |key, value|
+    i = 0 
+    while i < value.length do
+      new_hash[value[i]][:color].push(key.to_s)
+      i += 1
+    end
+    #binding.pry
+  end
+  
+  data[:lives].each do |key, value|
+    i = 0 
+    while i < value.length do
+     new_hash[value[i]][:lives] = [key]
+      i += 1
+    end
+    #binding.pry
+  end
+  
+  new_hash
 end
